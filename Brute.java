eeced281c8bd7d058585168600b8d7b81b0b0cc6
sanjay3004 <111427205+sanjay3004@users.noreply.github.com>
2022-08-24 12:01:38 +0530

    Add files via upload

---
+++ Brute.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Brute {
+   
+   static int flag=0;
+
+	static void CombinationRepetitionUtil(int chosen[], int arr[],
+			int index, int r, int start, int end,int amt) {
+
+		if (index == r) {
+		    int sum=0;
+			for (int i = 0; i < r; i++) {
+				sum+=arr[chosen[i]];
+			}
+			if(sum==amt){
+			    for(int i=0;i<r;i++){
+			    System.out.printf("%d ", arr[chosen[i]]);
+			    flag=1;
+			    }
+			}
+			return;
+		}
+
+        if(flag==1){
+            return;
+        }
+		for (int i = start; i <= end; i++) {
+			chosen[index] = i;
+			CombinationRepetitionUtil(chosen, arr, index + 1,
+					r, i, end,amt);
+		}
+		return;
+	}
+
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int arr[] = {1, 2, 5};
+		int n = arr.length;
+		int r = 3;
+		int chosen[] = new int[r + 1];
+		int amt;
+		System.out.println("Enter the amount: ");
+		amt=sc.nextInt();
+		CombinationRepetitionUtil(chosen, arr, 0, r, 0, n - 1,amt);
+	}
+}
+
