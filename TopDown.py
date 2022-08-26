93d1d9a331568576a11f672d958d538612edef78
sanjay3004 <111427205+sanjay3004@users.noreply.github.com>
2022-08-26 15:38:00 +0530

    Add files via upload

---
+++ TopDown.py
@@ -0,0 +1,19 @@
+def minimum_coins(coin_list, change):
+    min_coins = change
+    if change in coin_list:
+        return 1, [change]
+    else:
+        cl = []
+        for coin in coin_list:
+            if coin < change:
+                mt, t = minimum_coins(coin_list, change - coin)
+                num_coins = 1 + mt
+                if num_coins < min_coins:
+                    min_coins = num_coins
+                    cl = t + [coin]
+    return min_coins, cl
+
+change=int(input("enter the amount"))
+coin_list = [1,3,4,5]
+min, c = minimum_coins(coin_list, change)
+print("Total number of coins required is %s." % min, c) 
